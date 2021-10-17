c103477b428c0ee97d2a65895ba51986778b7e52
taziobaker <taziobaker@gmail.com>
2021-10-17 19:14:23 -0400

    Jiba jaba

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,32 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
 
+st.title("This is the title")
+
+st.header("This is a header")
+
+st.subheader("Subheader")
+
+st.write("This is regular text.")
+
+"""
+# header
+## subheader
+"""
+
+some_dictionary = {
+  "key": "value",
+  "key2": "value2"
+}
+
+some_list = [1,2,3]
+st.write(some_dictionary)
+st.write(some_list)
+
+st.sidebar.write("Write this to the sidebar")
+
+
+df = pd.DataFrame(np.random.randn(50, 20), columns=('col %d' % i for i in range(20)))
+
+st.DataFrame(df)
