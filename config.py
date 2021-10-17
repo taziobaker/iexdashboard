cb5a502f243e9b2b5f0c4f0c776efcaed1c03a3e
taziobaker <taziobaker@gmail.com>
2021-10-17 19:44:54 -0400

    Update 3

--- config.py
+++ config.py
@@ -1,17 +1,21 @@
-TWITTER_CONSUMER_KEY = ''
-TWITTER_CONSUMER_SECRET = ''
-TWITTER_ACCESS_TOKEN = ''
-TWITTER_ACCESS_TOKEN_SECRET = ''
+streamlit
+requests
+redis
 
-TWITTER_USERNAMES = [
-    'traderstewie',
-    'the_chart_life',
-    'canuck2usa',
-    'sunrisetrader',
-    'tmltrader'
-]
+#TWITTER_CONSUMER_KEY = ''
+#TWITTER_CONSUMER_SECRET = ''
+#TWITTER_ACCESS_TOKEN = ''
+#TWITTER_ACCESS_TOKEN_SECRET = ''
 
-DB_HOST = '127.0.0.1'
-DB_USER = 'postgres'
-DB_PASS = 'password'
-DB_NAME = 'etfdb'
+#TWITTER_USERNAMES = [
+#    'traderstewie',
+#    'the_chart_life',
+#    'canuck2usa',
+#    'sunrisetrader',
+#    'tmltrader'
+#]
+
+#DB_HOST = '127.0.0.1'
+#DB_USER = 'postgres'
+#DB_PASS = 'password'
+#DB_NAME = 'etfdb'
