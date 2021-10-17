704ad3ca424a42b45ea319162958b0b855f082c2
taziobaker <taziobaker@gmail.com>
2021-10-17 18:51:15 -0400

    Update 2 c

--- dashboard.py
+++ dashboard.py
@@ -5,4 +5,15 @@ st.title("This is the title")
 st.header("This is a header")
 
 st.subheader("Subheader")
+
 st.write("This is regular text.")
+
+"""
+# header
+## subheader
+"""
+
+{
+  "key": "value",
+  "key2": "value2"
+}
